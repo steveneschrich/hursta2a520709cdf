f46b5e9a7e02e2df30eb72f4ff1aedc92a018d7a
Eschrich, Steven <steven.eschrich@moffitt.org>
2020-03-30 21:09:14 -0400

    Initial package.

---
+++ create_package.R
@@ -0,0 +1,7 @@
+library("makecdfenv")
+make.cdf.package(filename="HuRSTA_2a520709.CDF",
+	author = "Steven Eschrich",
+	maintainer = "Steven Eschrich <Steven.Eschrich@moffitt.org>",
+	version = "1.0",
+	species = "Homo_sapiens"
+)
